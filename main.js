129063e723bf2a6e804b51a4d1a8ad5ad017f0ba
LUKAS SANTOS VENANCIO <21276375@cfp127aluno.local>
2022-02-09 16:51:53 -0300

    Adding to GitHub for future reference.

---
+++ main.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const semaforo = document.getElementById('semaforo')
+const verde = document.getElementById('verde')
+const amarelo = document.getElementById('amarelo')
+const vermelho = document.getElementById('vermelho')
+const automatico = document.getElementById('automatico')
+let idAutomatico = null
+
+// Funções para determinar em qual estado esta o semáforo
+const semaforoDesligado = () =>{
+    return semaforo.src.includes('desligado')
+}
+
+const semaforoVerde = () =>{
+    return semaforo.src.includes('verde')
+}
+
+const semaforoAmarelo = () =>{
+    return semaforo.src.includes('amarelo')
+}
+
+const semaforoVermelho = () =>{
+    return semaforo.src.includes('vermelho')
+}
+
+//Funções para ativar os botões
+const ligarVerde = () =>{
+    if(!semaforoDesligado())
+        semaforo.src = './img/verde.png'
+}
+
+const ligarAmarelo = () =>{
+    if(!semaforoDesligado())
+        semaforo.src = './img/amarelo.png'
+}
+
+const ligarVermelho = () =>{
+    if(!semaforoDesligado())
+        semaforo.src = './img/vermelho.png'
+}
+
+const desligar = () =>{
+    if(!semaforoDesligado())
+        semaforo.src = './img/desligado.png'
+    else
+        semaforo.src = './img/verde.png'       
+}
+
+const alternarCores = () => {
+    if(semaforoVerde())
+        ligarAmarelo()
+    else if(semaforoAmarelo())
+         ligarVermelho()     
+    else
+        ligarVerde()     
+}
+
+const ligarAutomatico = () =>{
+    if(idAutomatico == null){
+        idAutomatico = setInterval(alternarCores, 1000)
+        automatico.textContent = 'Parar'
+
+    }else{
+        clearInterval(idAutomatico)
+        automatico.textContent = 'Automático'
+        idAutomatico = null
+    }
+    
+}
+
+verde.addEventListener('click', ligarVerde)
+amarelo.addEventListener('click', ligarAmarelo)
+vermelho.addEventListener('click', ligarVermelho)
+semaforo.addEventListener('dblclick', desligar)
+automatico.addEventListener('click', ligarAutomatico)
